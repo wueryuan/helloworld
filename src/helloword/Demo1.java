05c96af7999101223d4019a0076fa2a85c5d9973
Administrator <Administrator@192.168.0.112>
2018-12-16 23:00:38 +0800

    数组练习

---
+++ src/helloword/Demo1.java
@@ -0,0 +1,19 @@
+package helloword;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] arr = new int [3]; 
+		int [] x = new int [] {1,2,3};
+		int [] y = {1,2,3};
+		
+		for(int a : y){
+			System.out.println(a);
+		}
+
+
+	}
+
+}
